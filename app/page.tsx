76c0a97367a02b3a41c92c5aa0fc9f95e878d3a7
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-08-18 18:25:43 -0300

    feat: build projects section

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { FloatingNav } from '@/components/ui/FloatingNav';
 
 import Image from 'next/image';
 import { FaHome } from 'react-icons/fa';
+import RecentProjects from '@/components/RecentProjects';
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
